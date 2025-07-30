cb09904ebe4f2d7bfee1c50d3722d8ed13510870
Gcvishwas <gcvishwas17@gmail.com>
2025-07-30 17:20:54 +0545

    Closed scanner

--- src/Basics/OddEvenCheck.java
+++ src/Basics/OddEvenCheck.java
@@ -3,15 +3,15 @@ package Basics;
 import java.util.Scanner;
 
 public class OddEvenCheck {
-    public static void main(String[] args){
-        Scanner scanner= new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter number : ");
         int num = scanner.nextInt();
-        if(num%2==0){
+        if (num % 2 == 0) {
             System.out.print("The number   : " + num + " is even");
-        }
-        else {
+        } else {
             System.out.print("The number   : " + num + " is odd");
         }
+        scanner.close();
     }
 }

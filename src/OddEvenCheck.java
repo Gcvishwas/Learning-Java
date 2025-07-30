ab86eedfa57479d08aafbecaae89304d6df8f2f7
Gcvishwas <gcvishwas17@gmail.com>
2025-07-30 16:28:48 +0545

    Third Program

---
+++ src/OddEvenCheck.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class OddEvenCheck {
+    public static void main(String[] args){
+        Scanner scanner= new Scanner(System.in);
+        System.out.print("Enter number : ");
+        int num = scanner.nextInt();
+        if(num%2==0){
+            System.out.print("The number   : " + num + " is even");
+        }
+        else {
+            System.out.print("The number   : " + num + " is odd");
+        }
+    }
+}

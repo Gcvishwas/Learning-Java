34e11333fca767c7af5ca73eff2fe1d759dfc465
Gcvishwas <gcvishwas17@gmail.com>
2025-07-31 15:07:42 +0545

    Program to check palindrome

---
+++ src/Basics/PalindromeCheck.java
@@ -0,0 +1,24 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int number = sc.nextInt();
+        int original = number;
+        int reversed = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+        if (original == reversed)
+            System.out.println(original + " is a palindrome");
+        else
+            System.out.println(original + " is not a palindrome");
+        sc.close();
+    }
+}

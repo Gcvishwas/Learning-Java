6c387255a22b7a4ea5baa4a45edf98b3f678f41f
Gcvishwas <gcvishwas17@gmail.com>
2025-08-28 14:19:34 +0545

    Program to demonstrate Border Layout

---
+++ src/Advanced_Java/Swing/BorderLayoutDemo.java
@@ -0,0 +1,24 @@
+package Advanced_Java.Swing;
+
+import javax.swing.*;
+
+import java.awt.BorderLayout;
+
+public class BorderLayoutDemo {
+    public static void main(String[] args) {
+
+        // Create Frame
+        JFrame frame = new JFrame("Vishwas GC Border");
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        frame.add(new JButton("North"), BorderLayout.NORTH);
+        frame.add(new JButton("South"), BorderLayout.SOUTH);
+        frame.add(new JButton("East"), BorderLayout.EAST);
+        frame.add(new JButton("West"), BorderLayout.WEST);
+        frame.add(new JButton("Center"), BorderLayout.CENTER);
+
+        frame.setVisible(true);
+    }
+}

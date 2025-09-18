13ee1d8d22a7ab5ead8950c9691c171a9c8323a6
Gcvishwas <gcvishwas17@gmail.com>
2025-09-18 21:58:20 +0545

    Interface implementation

---
+++ src/Advanced_Java/RMIExample/CalculatorImpl.java
@@ -0,0 +1,16 @@
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.RemoteException;
+
+public class CalculatorImpl extends UnicastRemoteObject implements Calculator {
+    protected CalculatorImpl() throws RemoteException {
+        super();
+    }
+
+    public int sum(int a, int b) throws RemoteException {
+        return a + b;
+    }
+
+    public int difference(int a, int b) throws RemoteException {
+        return a - b;
+    }
+}
